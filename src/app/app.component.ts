65af92c8cb1d043f93dc154ccb9cb0b526dd2d01
kartheek Reddy jonnalagadda <kartheekjonnalagadda@gmail.com>
2019-03-31 23:28:49 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+
+import * as firebase from "firebase";
+import { AngularFireAuth } from "@angular/fire/auth";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "oshop";
+  items: Observable<any[]>;
+  constructor(private afAuth: AngularFireAuth) {}
+  login() {
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "src/environments/environment";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
